737038ad2d30216a441ca54d2308a14696eff90a
Rhylie Ladoucer <gladouc@clemson.edu>
2026-03-11 11:18:11 -0400

    On branch main  Initial commit  Changes to be committed: 	new file:   src.py

---
+++ src.py
@@ -0,0 +1,7 @@
+from apify import Actor, Event
+from apify_client import ApifyClient
+from __future__ import annotations
+from crawlee.crawlers import BeautifulSoupCrawler, BeautifulSoupCrawlingContext
+import asyncio
+
+
